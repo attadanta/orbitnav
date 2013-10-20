4b310994a8b6a8aa729175392a7245ee27f26a78
Jonathan Merritt <j.s.merritt@gmail.com>
2013-10-20 14:05:56 +1100

    DragHandler javadoc.

--- src/main/java/org/arcball/internal/DragHandler.java
+++ src/main/java/org/arcball/internal/DragHandler.java
@@ -1,5 +1,19 @@
 package org.arcball.internal;
 
+/**
+ * Drag handler.  Handles dragging events for 3D interaction.
+ * <p>
+ * Implementors of this interface can be used in conjunction with {@link org.arcball.internal.DragHelper DragHelper} 
+ * to handle dragging events for 3D interaction.
+ * 
+ * @author Jonathan Merritt (<a href="mailto:j.s.merritt@gmail.com">j.s.merritt@gmail.com</a>)
+ */
 public interface DragHandler {
+    /**
+     * Handle a drag event.  This method is normally called by {@link org.arcball.internal.DragHelper DragHelper} when
+     * a drag has been detected.
+     * @param deltaX the change in mouse (scene) x coordinate
+     * @param deltaY the change in mouse (scene) y coordinate
+     */
     void handleDrag(double deltaX, double deltaY);
 }
